1cdf212f72f36807715facf24a73b9806224fc5d
sreyvoath <141004103+sreyvoath@users.noreply.github.com>
2024-04-29 10:26:35 +0700

    update code Airport_information

--- src/AirLines/Airline.ts
+++ src/AirLines/Airline.ts
@@ -1,29 +1,19 @@
 import { Airport } from "./Airport";
 
-
-export class Airline {
-    
-    constructor(private name: string, private code: string,private airPorts: Airport[]) {
-        this.name=name;
-        this.code=code;
-        this.airPorts = [];
-
+export class Airline{
+    constructor(private name:string,private code:string,private airPorts:Airport[]){
+        this.name =name;
+        this.code =code;
+        this.airPorts=[];
     }
-
-    getName(): string {
+    getName(){
         return this.name;
     }
-
-    getCode(): string {
-        return this.code;
+    getCode(){
+       return this.code;
     }
-
-    addAirport(airPort: Airport): void {
+    addAirport(airPort:Airport):void{
         this.airPorts.push(airPort);
     }
-    removeAirport(airportCode: string): void {
-        this.airPorts = this.airPorts.filter(airport => airport.getCode() !== airportCode);
-    }
 }
-
 
