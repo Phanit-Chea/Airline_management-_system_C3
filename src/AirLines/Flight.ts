f38cf27cad9a65af6623e584ab4f68f5f1ac3bee
kos-koeuk <keourk.koh@student.passerellesnumeriques.org>
2024-04-30 20:12:51 +0700

    manage_mealOfFlight

--- src/AirLines/Flight.ts
+++ src/AirLines/Flight.ts
@@ -1,7 +1,10 @@
 import { Route } from "../Routes/Route";
+import { Chef } from "../FlightReservation/Chef";
+import { Meal } from "../FlightReservation/Meal";
+import { TypeFood } from "../enums/TypeFood";
 
 export class Flight{
-    constructor(private flightID:number,private departureTime:string, private arriveTime:string,private DurationMenute:string,private route:Route){
+    constructor(private flightID:number,private departureTime:string, private arriveTime:string,private DurationMenute:string,private Chef:Chef ,private route:Route){
         this.flightID = flightID;
         this.departureTime = departureTime;
         this.arriveTime = arriveTime;
@@ -31,4 +34,21 @@ export class Flight{
     setDurationMenute(DurationMenute:string):void{
         this.DurationMenute = DurationMenute;
     }
+    getChef (Chef:Chef) {
+        this.Chef.nameChef;
+    }
 }
+let route = new Route(12,"2000 km","12-02-2023","22-02-2023")
+let meal = new Meal (200);
+meal.setQuantityOfMeal(TypeFood.GLUTENFREE,20);
+meal.setQuantityOfMeal(TypeFood.VEGAN, 20);
+meal.setQuantityOfMeal(TypeFood.GLUTENFREE, 50);
+meal.setQuantityOfMeal(TypeFood.OTHER, 30);
+
+let Chefs = new Chef("koeuk",meal );
+
+let flight= new Flight(12,"12-02-2023","22-02-2023","10 Days",Chefs,route)
+
+console.log(flight);
+
+
--- src/enums/CrewType.ts
+++ src/enums/CrewType.ts
@@ -4,4 +4,7 @@ export enum CrewType {
      PILOT = " Pilot",
      CO_PILOT = "CO_Pilot",
      FLIGHTATTENDANT = "FlightAttendat",
+     CHEF="Chef"
+
 }
+
